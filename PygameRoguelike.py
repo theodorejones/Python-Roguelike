e9ca732ff40c1e34cfaf8a05ce278277ba0d7be8
theodorejones <70782338+theodorejones@users.noreply.github.com>
2021-01-18 16:09:27 -0500

    Add files via upload
    Added vertical movement

--- PygameRoguelike.py
+++ PygameRoguelike.py
@@ -2,8 +2,9 @@ import pygame
 
 pygame.init()
 
-display_width = 800
-display_height = 600
+pixel = 128
+display_width = 3 * pixel
+display_height = 3 * pixel
 
 black = (0,0,0)
 white = (255,255,255)
@@ -17,16 +18,17 @@ clock = pygame.time.Clock()
 
 carImg = pygame.image.load('person.png')
 
-def car(x,y):
+def player(x,y):
     gameDisplay.blit(carImg,(x,y))
 
 
 
 def game_loop():
-    x = (display_width * 0.45)
-    y = (display_height * 0.8)
+    x = (2*pixel)
+    y = (2*pixel)
 
     x_change = 0
+    y_change = 0
 
     gameExit = False
 
@@ -38,25 +40,32 @@ def game_loop():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    x_change = -5
+                    x_change = -pixel
                 if event.key == pygame.K_RIGHT:
-                    x_change = 5
+                    x_change = pixel
+                if event.key == pygame.K_UP:
+                    y_change = -pixel
+                if event.key == pygame.K_DOWN:
+                    y_change = pixel
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_change = 0
 
         x += x_change
+        y += y_change
 
         gameDisplay.fill(white)
-        car(x,y)
+        player(x,y)
 
         if x > display_width - car_width or x < 0:
             gameExit = True
             
         
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(10)
 
 
 
