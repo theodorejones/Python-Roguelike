a5bf5590030d2ad8bd2c23998e67d9e1d1cbff01
theodorejones <70782338+theodorejones@users.noreply.github.com>
2021-01-23 11:32:37 -0500

    Add files via upload
    Unstable version of the playfield

--- PygameRoguelike.py
+++ PygameRoguelike.py
@@ -15,13 +15,18 @@ car_width = 73
 car_height = 73
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('Will Of The Dungeon')
 clock = pygame.time.Clock()
 
-carImg = pygame.image.load('person.png')
+person = pygame.image.load('person.png')
+monster = pygame.image.load('monster.png')
+sword = pygame.image.load('sword.png')
+shield = pygame.image.load('shield.png')
+wall = pygame.image.load('wall.png')
 
 def player(x,y):
-    gameDisplay.blit(carImg,(x,y))
+    gameDisplay.blit(person,(x,y))
+
 field=[]
 for x in range(x_field):
     field.append([])
@@ -33,12 +38,17 @@ and the game will make decisions based on the given statistics. For instance, a
 Sword or Shield will raise a character's Attack or Health, respectively, and a
 Monster will attack the player as it passes if the player's Attack isn't enough to
 kill it'''
-
+def swap(field, x1, y1, x2, y2):
+    temp=field[x1][y1]
+    field[x1][y1]=field[x2][y2]
+    field[x2][y2]=temp
+    return field
 
 def game_loop():
-    x = (2*pixel)
-    y = (2*pixel)
-
+    global field
+    field[1][1]=[1,0,0]
+    x=1
+    y=1
     x_change = 0
     y_change = 0
 
@@ -52,13 +62,13 @@ def game_loop():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    x_change = -pixel
+                    field=swap(field,x,y,x-1,y)
                 if event.key == pygame.K_RIGHT:
-                    x_change = pixel
+                    field=swap(field,x,y,x+1,y)
                 if event.key == pygame.K_UP:
-                    y_change = -pixel
+                    field=swap(field,x,y,x,y-1)
                 if event.key == pygame.K_DOWN:
-                    y_change = pixel
+                    field=swap(field,x,y,x,y+1)
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -70,7 +80,13 @@ def game_loop():
         y += y_change
 
         gameDisplay.fill(white)
-        player(x,y)
+        for x in range(x_field):
+            for y in range(y_field):
+                if(field[x][y][0] == 1):
+                    print(x)
+                    print(y)
+                    print(field)
+                    gameDisplay.blit(person,(128*x,128*y))
 
         if x > display_width - car_width or x < 0:
             gameExit = True
