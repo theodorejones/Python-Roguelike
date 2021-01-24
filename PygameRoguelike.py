17d75dbe81fbab60f8f7a55b3a4f6626644fbd9a
theodorejones <70782338+theodorejones@users.noreply.github.com>
2021-01-24 17:03:47 -0500

    Add files via upload
    Buggy version of the playfield

--- PygameRoguelike.py
+++ PygameRoguelike.py
@@ -29,30 +29,29 @@ def player(x,y):
 
 field=[]
 for x in range(x_field):
-    field.append([])
     for y in range(y_field):
-        field[x].append([0,0,0])
+        field.append(0)
 #ID, health, and attack, in that order, per square
-'''When the player moves, the current and destination squares will swap,
-and the game will make decisions based on the given statistics. For instance, a
-Sword or Shield will raise a character's Attack or Health, respectively, and a
-Monster will attack the player as it passes if the player's Attack isn't enough to
-kill it'''
-def swap(field, x1, y1, x2, y2):
-    temp=field[x1][y1]
-    field[x1][y1]=field[x2][y2]
-    field[x2][y2]=temp
+
+def swap(field, pos1, pos2):
+    print(field)
+    field[pos1],field[pos2]=field[pos2],field[pos1]
+    print(field)
     return field
 
 def game_loop():
     global field
-    field[1][1]=[1,0,0]
     x=1
     y=1
-    x_change = 0
-    y_change = 0
+    print(x)
+    print(y)
+    pos=y*x_field+x
+    field[pos]=1
+    print(pos)
+    print(field)
 
     gameExit = False
+    pressed = False
 
     while not gameExit:
 
@@ -60,32 +59,54 @@ def game_loop():
             if event.type == pygame.QUIT:
                 gameExit = True
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    field=swap(field,x,y,x-1,y)
-                if event.key == pygame.K_RIGHT:
-                    field=swap(field,x,y,x+1,y)
-                if event.key == pygame.K_UP:
-                    field=swap(field,x,y,x,y-1)
-                if event.key == pygame.K_DOWN:
-                    field=swap(field,x,y,x,y+1)
+            try:
+                if event.type == pygame.KEYDOWN and pressed == False:
+                    if event.key == pygame.K_LEFT:
+                        field=swap(field,pos,pos-1)
+                        x=x-1
+                        print(x)
+                        print(y)
+                        pos=(y*x_field-1)+x
+                        print(pos)
+                        print(field)
+                    if event.key == pygame.K_RIGHT:
+                        field=swap(field,pos,pos+1)
+                        x=x+1
+                        print(x)
+                        print(y)
+                        pos=(y*x_field-1)+x
+                        print(pos)
+                        print(field)
+                    if event.key == pygame.K_UP:
+                        field=swap(field,pos,pos-x_field)
+                        y=y-1
+                        print(x)
+                        print(y)
+                        pos=(y*x_field-1)+x
+                        print(pos)
+                        print(field)
+                    if event.key == pygame.K_DOWN:
+                        field=swap(field,pos,pos+x_field)
+                        y=y+1
+                        print(x)
+                        print(y)
+                        pos=(y*x_field-1)+x
+                        print(pos)
+                        print(field)
+                    pressed = True
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    x_change = 0
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    y_change = 0
-
-        x += x_change
-        y += y_change
-
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        pressed = False
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        pressed = False
+            except:
+                print("Boundary")
         gameDisplay.fill(white)
         for x in range(x_field):
             for y in range(y_field):
-                if(field[x][y][0] == 1):
-                    print(x)
-                    print(y)
-                    print(field)
+                pos=(y*x_field-1)+x
+                if(field[pos] == 1):
                     gameDisplay.blit(person,(128*x,128*y))
 
         if x > display_width - car_width or x < 0:
